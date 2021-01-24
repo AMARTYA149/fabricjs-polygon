42769ae33fb4a4f912d44297631d7f1181748fc7
Amartya Aishwarya <amartya@koverhoop.com>
2021-01-24 14:03:14 +0530

    canvas html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FabricCanvasComponent } from './fabric-canvas/fabric-canvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FabricCanvasComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/fabric-canvas/fabric-canvas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fabric-canvas',
+  templateUrl: './fabric-canvas.component.html',
+  styleUrls: ['./fabric-canvas.component.css']
+})
+export class FabricCanvasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
